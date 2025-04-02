fc8f8b4cb559cf45a2bb3aba288b24b33619caa9
Elvin <harriselvin6@gmail.com>
2025-04-02 09:09:11 +0200

    secured the api key

--- Script/script.js
+++ Script/script.js
@@ -2,18 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, query, orderByChild, remove } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
-// Firebase Configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyBmNhbCmvNanVHVi2eSkGM6x5NOxbNDK6o",
-	authDomain: "word-finder-leaderboard.firebaseapp.com",
-	databaseURL: "https://word-finder-leaderboard-default-rtdb.firebaseio.com",
-	projectId: "word-finder-leaderboard",
-	storageBucket: "word-finder-leaderboard.firebasestorage.app",
-	messagingSenderId: "546576063050",
-	appId: "1:546576063050:web:a15b99187cbb19cf60a4b3",
-	measurementId: "G-FWNLBBHDQN"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);

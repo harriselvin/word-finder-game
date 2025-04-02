185b3355b94e10711d5a4f8109ac850d55757bc8
Elvin <harriselvin6@gmail.com>
2025-04-02 16:30:12 +0200

    changed the gitignore file

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBmNhbCmvNanVHVi2eSkGM6x5NOxbNDK6o",
+	authDomain: "word-finder-leaderboard.firebaseapp.com",
+	databaseURL: "https://word-finder-leaderboard-default-rtdb.firebaseio.com",
+	projectId: "word-finder-leaderboard",
+	storageBucket: "word-finder-leaderboard.firebasestorage.app",
+	messagingSenderId: "546576063050",
+	appId: "1:546576063050:web:a15b99187cbb19cf60a4b3",
+	measurementId: "G-FWNLBBHDQN"
+};
